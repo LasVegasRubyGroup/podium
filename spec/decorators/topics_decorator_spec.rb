69282f290f8655d064f9f4a89eb380079c8dcae7
alexpeachey <alex.peachey@gmail.com>
2014-02-20 12:07:15 -0800

    List Topics Specifications
    * Two specs, one passing, one pending
    * Need to implement CreateTopic service

---
+++ spec/decorators/topics_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'decorator_spec_helper'
+
+describe TopicsDecorator do
+end
---
+++ spec/features/topics/list_topics_spec.rb
@@ -0,0 +1,34 @@
+require 'feature_spec_helper'
+
+describe 'Listing Topics' do
+  context 'on the home page' do
+    let(:home_page) { root_path }
+
+    context 'when there are no topics in the system' do
+      it 'shows the topics header with no topics' do
+        visit home_page
+        expect(current_path).to eq root_path
+        expect(page).to have_selector('.topics-title', text: I18n.t('topics.index.title'))
+        expect(page).not_to have_selector('.topic')
+      end
+    end
+
+    context 'when there are 3 topics in the system' do
+      before do
+        # CreateTopic.new(title: 'Topic 1', description: 'First Topic').call
+        # CreateTopic.new(title: 'Topic 2', description: 'Second Topic').call
+        # CreateTopic.new(title: 'Topic 3', description: 'Third Topic').call
+      end
+
+      it 'shows the topcis header with 3 topics' do
+        pending 'Implementation of CreateTopic needed'
+        visit home_page
+        expect(current_path).to eq root_path
+        expect(page).to have_selector('.topics-title', text: I18n.t('topics.index.title'))
+        expect(page).to have_selector('.topic'), text: 'Topic 1'
+        expect(page).to have_selector('.topic'), text: 'Topic 2'
+        expect(page).to have_selector('.topic'), text: 'Topic 3'
+      end
+    end
+  end
+end
