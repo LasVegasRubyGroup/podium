a607a689437817b49eebc10c18f3a86de5a79c4c
Jan-Hettich <janhettich@gmail.com>
2014-07-27 00:35:00 -0700

    restore orig version of application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Podium
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.autoload_paths += %W(#{config.root}/lib)
+ #   config.autoload_paths += %W(#{config.root}/lib)
     
     config.sass.preferred_syntax = :sass
   end
