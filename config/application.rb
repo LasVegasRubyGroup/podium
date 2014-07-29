1c29e279d53a4a03a5ce7a93c4337dad761133e5
Jan-Hettich <janhettich@gmail.com>
2014-07-28 22:47:48 -0700

    changes for Rails 4.1 based on comparison to freshly generated Rails 4.1 project

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,17 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Podium
   class Application < Rails::Application
@@ -20,8 +27,7 @@ module Podium
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
- #   config.autoload_paths += %W(#{config.root}/lib)
-    
     config.sass.preferred_syntax = :sass
   end
 end
+
