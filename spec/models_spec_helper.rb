5bf48ecc1a9581c3033a09ab31e4385b63369a72
Jan-Hettich <janhettich@gmail.com>
2014-07-29 00:00:22 -0700

    specs running and passing with Rails 4.1; added trivial Topic spec to models

--- spec/models_spec_helper.rb
+++ spec/models_spec_helper.rb
@@ -1,11 +1,14 @@
 require 'base_spec_helper'
 require 'active_record'
 require 'rspec/rails/extensions/active_record/base'
-require 'shoulda'
+
 Dir[File.join(RAILS_ROOT, 'spec/support_models/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Base.establish_connection(
   YAML.load(File.read(RAILS_ROOT + '/config/database.yml'))['test']
 )
 
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
 ActiveSupport::Dependencies.autoload_paths << "#{RAILS_ROOT}/app/models"
+ActiveSupport::Dependencies.autoload_paths << "#{RAILS_ROOT}/app/models/concerns"
